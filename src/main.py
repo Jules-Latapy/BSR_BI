7b877aaf625df4e84cb2eba953b5881fe616ae6f
Jules-Latapy <jullatapy@gmail.com>
2024-05-23 12:45:25 +0200

    typage

--- src/main.py
+++ src/main.py
@@ -41,65 +41,71 @@ class SansObjet:
             return self.proba_plein_global() >= 0.50
 
 
+class State:
+    def __init__(self, nbr_plein, nbr_vide, objets, objets_donneur, vie, vie_donneur):
+        self.nbr_plein = nbr_plein
+        self.nbr_vide = nbr_vide
+        self.objets_donneur = objets_donneur
+        self.objets = objets
+        self.nbr_vie_max = vie
+        self.vie = vie
+        self.vie_donneur = vie_donneur
+        # --
+        self.incertitude = 0
+        self.chance_perdre1 = 0
+        self.chance_donneur_perdre1 = 0
+
+    def proba_plein_global(self):
+        return self.nbr_plein / (self.nbr_vide + self.nbr_plein)
+
+    def doit_suicider(self):
+        return self.proba_plein_global() < 50
+
+    def doit_tirer(self):
+        return self.proba_plein_global() >= 50
+
+    def roll(self):
+        for o in self.objets:
+            print(o.utilise(self))
+
+
 class AvecObjet:
-    class State:
-        def __init__(self, nbr_plein, nbr_vide, objets, objets_donneur, vie, vie_donneur):
-            self.nbr_plein = nbr_plein
-            self.nbr_vide = nbr_vide
-            self.objets_donneur = objets_donneur
-            self.objets = objets
-            self.vie = vie
-            self.vie_donneur = vie_donneur
-
-        def proba_plein_global(self):
-            return self.nbr_plein / (self.nbr_vide + self.nbr_plein)
-
-        def doit_suicider(self):
-            return self.proba_plein_global() < 50
-
-        def doit_tirer(self):
-            return self.proba_plein_global() >= 50
-
-        def roll(self):
-            for o in self.objets:
-                o.utilise(self)
-
     class Objet:
-        def utilise(state):
+        def utilise(self, state: State) -> int:
             raise "unimplemented"
 
     class Telephone(Objet):
-        def utilise(state):
+        def utilise(self, state) -> int:
             pass
 
     class Pilule(Objet):
-        def utilise(state):
+        def utilise(self, state) -> int:
             pass
 
     class Voleur(Objet):
-        def utilise(state):
+        def utilise(self, state) -> int:
             pass
 
     class Inverseur(Objet):
-        def utilise(state):
+        def utilise(self, state) -> int:
             pass
 
     class Clope(Objet):
-        def utilise(state):
+        def utilise(self, state) -> int:
             pass
 
     class Biere(Objet):
-        def utilise(state):
+        def utilise(self, state) -> int:
             pass
 
     class Loupe(Objet):
-        def utilise(state):
+        def utilise(self, state) -> int:
             pass
 
     class Scie(Objet):
-        def utilise(state):
+        def utilise(self, state) -> int:
             pass
 
     class Menotte(Objet):
-        def utilise(state):
+        def utilise(self, state) -> int:
             pass
--- src/regles.md
+++ src/regles.md
@@ -7,11 +7,21 @@
 ## Objet a prendre
 
 - telephone
+  - regarde une cartouche (100%/0%)
 - pilule
+  - une chance d'avoir -1
+  - une chance d'avoir +2
 - voleurs
+  - utilise un objet du donneur
 - inverseur
+  - inverse la cartouche actuelle
 - clope
+  - regenere une vie sauf si plein
 - biere
-- loop
+  - vide la cartouche
+- loupe
+  - regarde la cartouche en cours(100%/0%)
 - scie
+  - on augmente les degats*2
 - menotte
+  - si on tire sur le donneur, on continue de jouer
